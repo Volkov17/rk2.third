c2220c73f822d8a66ed8b3b176b040bbb5e3b6e8
Volkov17 <32384533+Volkov17@users.noreply.github.com>
2017-11-12 20:55:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int n, i, j, a, b, t;
+    int n, i, j, a, b, t,str,stolb;
     cin >> a >> b;
     int arr[a][b];
     for (i = 0; i < a; i++)
@@ -11,46 +11,47 @@ int main()
             cin >> arr[i][j];
     i = j = 0;
     cin >> n;
-    for (t = 0; t < n; t++)
-        while (true) {
-            swap(arr[0][0], arr[i][j]);
-            if (i == 0 && j == b - 1) {
-                i++;
-                continue;
+    str= sizeof(arr)/sizeof(arr[0]);
+    stolb = sizeof(arr[0])/sizeof(arr[0][0]);
+    if ((a==str) && (b==stolb)) {
+        for (t = 0; t < n; t++)
+            while (true) {
+                swap(arr[0][0], arr[i][j]);
+                if (i == 0 && j == b - 1) {
+                    i++;
+                    continue;
+                } else if (i == 0) {
+                    j++;
+                    continue;
+                }
+                if (i == a - 1 && j == b - 1) {
+                    j--;
+                    continue;
+                } else if (j == b - 1) {
+                    i++;
+                    continue;
+                }
+                if (i == a - 1 && j == 0) {
+                    i--;
+                    continue;
+                } else if (i == a - 1) {
+                    j--;
+                    continue;
+                }
+                if (i == 1 && j == 0) {
+                    i = j = 0;
+                    break;
+                } else if (j == 0) {
+                    i--;
+                    continue;
+                }
             }
-            else if (i == 0) {
-                j++;
-                continue;
-            }
-            if (i == a - 1 && j == b - 1) {
-                j--;
-                continue;
-            }
-            else if (j == b - 1) {
-                i++;
-                continue;
-            }
-            if (i == a - 1 && j == 0) {
-                i--;
-                continue;
-            }
-            else if (i == a - 1) {
-                j--;
-                continue;
-            }
-            if (i == 1 && j == 0) {
-                i = j = 0;
-                break;
-            }
-            else if (j == 0) {
-                i--;
-                continue;
-            }
+        for (i = 0; i < a; i++) {
+            for (j = 0; j < b; j++)
+                cout << arr[i][j] << " ";
+            cout << endl;
         }
-    for (i = 0; i < a; i++) {
-        for (j = 0; j < b; j++)
-            cout << arr[i][j] << " ";
-        cout << endl;
     }
+    else cout <<"error";
     return 0;
 }
